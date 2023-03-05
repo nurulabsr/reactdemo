1a1d01877360fb15ffe528e0c8efe8b88885cfe8
ruabsarnul <122334016+ruabsarnul@users.noreply.github.com>
2023-03-05 22:25:33 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { Routes, Route, BrowserRouter} from "react-router-dom"
 import Home from './components/Pages/Home/Home';
 import HomeDetails from './components/Pages/DelailsPage/HomeDetails';
+import Error from './components/Pages/Error/Error';
 function App() {
   return (
     <BrowserRouter className="App">
     <Routes>
       <Route exect path="/" element={<Home />} />
       <Route exect path ="/details" element={<HomeDetails/>}></Route>
+      <Route path="*" element={<Error/>} />
     </Routes >
     </BrowserRouter>
   );
---
+++ src/components/Pages/Error/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+           <h1> 404, try again. The page does not exist!</h1> 
+        </div>
+    );
+};
+
+export default Error;
